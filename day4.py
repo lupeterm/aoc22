5140e68fef97650e052c3ea4427c389fc8925f12
Lukas Petermann <lukas.petermann13@gmail.com>
2022-12-04 10:11:57 +0100

    clean day4

--- day4.py
+++ day4.py
@@ -1,4 +1,4 @@
 # part 1
-print(len(list(filter(lambda x: (x[0] <= x[2] <= x[3] <= x[1]) or (x[2] <= x[0] <= x[1] <= x[3]) ,[[int(x) for x in line.split('-')] for line in open('4.in').read().replace(',','-').split("\n")]))))
+print(sum(map(lambda x: 1 if (x[0] <= x[2] <= x[3] <= x[1])   or (x[2] <= x[0] <= x[1] <= x[3])   else 0, [list(map(int, line.split('-'))) for line in open('4.in').read().replace(',','-').split("\n")])))
 # part 2 
-print(len(list(filter(lambda x: (x[0] <= x[2] <= x[1]) or (x[2] <= x[0] <= x[3]) or (x[2] <= x[1] <= x[3]) or (x[0] <= x[3] <= x[1]),[[int(x) for x in line.split('-')] for line in open('4.in').read().replace(',','-').split("\n")]))))
+print(sum(map(lambda x: 1 if (x[0] <= (x[2] or x[3]) <= x[1]) or (x[2] <= (x[0] or x[1]) <= x[3]) else 0, [list(map(int, line.split('-'))) for line in open('4.in').read().replace(',','-').split("\n")])))
